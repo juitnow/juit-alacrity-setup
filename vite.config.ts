0d60effecb6530106eafebf3eb049dc5029b1079
Pier Fumagalli <pier@usrz.com>
2024-09-26 12:17:20 +0200

    Fix base URL from pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL,
   clearScreen: false,
   plugins: [
     vue({ template: { transformAssetUrls } }),
